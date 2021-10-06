143a192189b5c935ed773db929750c635328ad18
fredikw <fred.i.kwizera@gmail.com>
2021-10-06 14:52:47 +0200

    edit-first-problem

--- regression/first_problem.py
+++ regression/first_problem.py
@@ -11,20 +11,24 @@ Xdata = np.load(os.getcwd() + "/regression/training_set/Xtrain_Regression_Part1.
 Ydata = np.load(os.getcwd() + "/regression/training_set/Ytrain_Regression_Part1.npy")
 testXdata = np.load(os.getcwd()+ "/regression/test_set/Xtest_Regression_Part1.npy")
 
+# Linear model
 # Training
 XdataTraining = Xdata[:slice_at]
 YdataTraining = Ydata[:slice_at]
-model = linear_model.LinearRegression()
-model.fit(XdataTraining,YdataTraining)
+linearModel = linear_model.LinearRegression()
+linearModel.fit(XdataTraining,YdataTraining)
 
 # Validation
 XdataValidation = Xdata[slice_at:]
 YdataValidation = Ydata[slice_at:]
 
-prediction = model.predict(XdataValidation)
+prediction = linearModel.predict(XdataValidation)
 error = mean_squared_error(prediction,YdataValidation)     # Linear model yields a small error of Approx. 0.024. This indicates that the model makes good predictions
 
 # Testing
-testPrediction = model.predict(testXdata)
+testPrediction = linearModel.predict(testXdata)
 
 #np.save('data.npy', testOutput)
+
+# TODO Try different models
+# TODO Find features with linear correlation
