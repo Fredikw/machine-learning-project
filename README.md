15330cf9bd6870a97b64395392c02066d50ac964
fredikw <fred.i.kwizera@gmail.com>
2021-10-11 16:42:13 +0200

    added multiple regressors

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # machine-learning-project
 Machine Learning - AAut project, Instituto Superior Técnico
+
+
+Frist Problem
+--------------------
+
+Performance measure
+
+https://medium.com/analytics-vidhya/mae-mse-rmse-coefficient-of-determination-adjusted-r-squared-which-metric-is-better-cd0326a5697e
+
+USE 'k fold cross validation', 'leave one out cross validation'
+
+10 popular regression methods
+https://www.educative.io/blog/scikit-learn-cheat-sheet-classification-regression-methods
+
+LightGBM
+https://www.analyticssteps.com/blogs/what-light-gbm-algorithm-how-use-it
+Not apropriate for this problem as it requires a training set of size 10,000+. It can easily overfit small data due to its sensitivity.
+
+XGBRegressor
+https://www.analyticssteps.com/blogs/introduction-xgboost-algorithm-classification-and-regression
+One of the reasons for good performance is Regularization. A technique that is used to get rid of overfitting of the model.
+https://www.datatechnotes.com/2019/06/regression-example-with-xgbregressor-in.html
+
+CatBoost Regressor
+https://towardsdatascience.com/catboost-regression-in-6-minutes-3487f3e5b329
+
+SGDRegressor
+
+KernelRidge
+https://scikit-learn.org/stable/modules/generated/sklearn.kernel_ridge.KernelRidge.html
+
+
+Elastic Net Regression
+https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.ElasticNet.html
--- regression/first_problem.py
+++ regression/first_problem.py
@@ -1,34 +1,123 @@
 import numpy as np
 import os
+import random
 
+import xgboost as xgb
+
+from sklearn.model_selection import train_test_split
 from sklearn import linear_model
+#from catboost import CatBoostRegressor
+from sklearn.linear_model import SGDRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import BayesianRidge
+from sklearn.ensemble import GradientBoostingRegressor
+
+from sklearn.svm import SVR
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+
 from sklearn.metrics import mean_squared_error
 
-slice_at = 90
-
 # Data load
-Xdata = np.load(os.getcwd() + "/regression/training_set/Xtrain_Regression_Part1.npy")
-Ydata = np.load(os.getcwd() + "/regression/training_set/Ytrain_Regression_Part1.npy")
-testXdata = np.load(os.getcwd()+ "/regression/test_set/Xtest_Regression_Part1.npy")
+x_data = np.load(os.getcwd() + "/training_set/Xtrain_Regression_Part1.npy")
+y_data = np.load(os.getcwd() + "/training_set/Ytrain_Regression_Part1.npy")
+
+x_training_set, x_test_set, y_training_set, y_test_set = train_test_split(x_data, y_data, test_size=10)
 
 # Linear model
-# Training
-XdataTraining = Xdata[:slice_at]
-YdataTraining = Ydata[:slice_at]
+
 linearModel = linear_model.LinearRegression()
-linearModel.fit(XdataTraining,YdataTraining)
+linearModel.fit(x_training_set,y_training_set)
 
-# Validation
-XdataValidation = Xdata[slice_at:]
-YdataValidation = Ydata[slice_at:]
+y_pred_linear = linearModel.predict(x_test_set)
+mse_linear = mean_squared_error(y_test_set, y_pred_linear)    #0.009726150151957687
 
-prediction = linearModel.predict(XdataValidation)
-error = mean_squared_error(prediction,YdataValidation)     # Linear model yields a small error of Approx. 0.024. This indicates that the model makes good predictions
+# XGBoost Regressor
 
-# Testing
-testPrediction = linearModel.predict(testXdata)
+XGBoostModel = xgb.XGBRegressor(verbosity=0)
+XGBoostModel.fit(x_training_set, y_training_set)
 
-#np.save('data.npy', testOutput)
+y_pred_XGBoost = XGBoostModel.predict(x_test_set)
+mse_xgbr = mean_squared_error(y_test_set, y_pred_XGBoost)
 
-# TODO Try different models
-# TODO Find features with linear correlation
+# CatBoost Regressor
+
+'''
+CatBoostModel = CatBoostRegressor(loss_function='RMSE')
+CatBoostModel.fit(x_training_set, y_training_set)
+
+y_pred_CatBoost = CatBoostModel.predict(x_test_set)
+mse_catBoost = mean_squared_error(y_test_set, y_pred_CatBoost)
+'''
+
+# Stochastic Gradient Descent Regression
+
+SGDRegressorModel = SGDRegressor()
+SGDRegressorModel.fit(x_training_set, np.ravel(y_training_set))
+
+y_pred_SGDRegressor = SGDRegressorModel.predict(x_test_set)
+mse_SGDRegressor = mean_squared_error(y_test_set, y_pred_SGDRegressor)
+
+# Kernel Ridge Regression
+
+KernelRidgeModel = KernelRidge(alpha=1.0)
+KernelRidgeModel.fit(x_training_set, y_training_set)
+
+y_pred_KernelRidge = KernelRidgeModel.predict(x_test_set)
+mse_KernelRidge = mean_squared_error(y_test_set, y_pred_KernelRidge)
+
+
+# Elastic Net Regression
+
+ElasticNetModel = ElasticNet(random_state=0)
+ElasticNetModel.fit(x_training_set, y_training_set)
+
+y_pred_ElasticNet = ElasticNetModel.predict(x_test_set)
+mse_ElasticNet = mean_squared_error(y_test_set, y_pred_ElasticNet)
+
+# Bayesian Ridge Regression
+
+BayesianRidgeModel = BayesianRidge()
+BayesianRidgeModel.fit(x_training_set, np.ravel( y_training_set))
+
+y_pred_BayesianRidge = BayesianRidgeModel.predict(x_test_set)
+mse_BayesianRidge = mean_squared_error(y_test_set, y_pred_BayesianRidge)
+
+
+# Gradient Boosting Regression
+
+GBRegressorModel = GradientBoostingRegressor(random_state=0)
+GBRegressorModel.fit(x_training_set, np.ravel( y_training_set))
+
+y_pred_GradientBoosting = GBRegressorModel.predict(x_test_set)
+mse_GradientBoosting = mean_squared_error(y_test_set, y_pred_GradientBoosting)
+
+# Support Vector Machine
+
+SVRModel = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=0.2))
+SVRModel.fit(x_training_set, np.ravel( y_training_set))
+
+y_pred_SVR = SVRModel.predict(x_test_set)
+mse_SVR = mean_squared_error(y_test_set, y_pred_SVR)
+
+
+print('------------------------------MSE of Regressors------------------------------')
+print('MSE of linear Regressor:         ', mse_linear)
+print('MSE of XGBoost Regressor:        ', mse_xgbr)
+#print('MSE of CatBoost Regressor:       ', mse_catBoost)
+print('MSE of SGD Regressor:            ', mse_SGDRegressor)
+print('MSE of KernelRidge Regressor:    ', mse_KernelRidge)
+print('MSE of ElasticNet Regressor:     ', mse_ElasticNet)
+print('MSE of BayesianRidge Regressor:  ', mse_BayesianRidge)
+print('MSE of GB Regressor:             ', mse_GradientBoosting)
+print('MSE of SVR Regressor:            ', mse_SVR)
+
+
+
+# TODO Find methodes for evaluation data
+
+# testXdata = np.load(os.getcwd()+ "/regression/test_set/Xtest_Regression_Part1.npy")
+# Predict y with best model
+# np.save('data.npy', testOutput)
