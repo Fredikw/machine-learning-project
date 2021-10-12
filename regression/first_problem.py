176f6d402b19fb418bb45c8a7b273bd3e9868612
fredikw <fred.i.kwizera@gmail.com>
2021-10-12 17:46:20 +0200

    minor refact

--- regression/first_problem.py
+++ regression/first_problem.py
@@ -4,25 +4,27 @@ import numpy as np
 from xgboost import XGBRegressor
 from sklearn.svm import SVR
 import pandas as pd
-#from catboost import CatBoostRegressor
+from catboost import CatBoostRegressor
 
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.linear_model import ElasticNet
 from sklearn.linear_model import BayesianRidge
-
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+#from sklearn.metrics import mean_squared_error
+#from sklearn.model_selection import train_test_split
 
 from os import getcwd
-
-from pygam import LinearGAM
-
+from matplotlib.pyplot import show
+from seaborn import heatmap
 
 # Data load
 x_data = np.load(getcwd() + "/regression/training_set/Xtrain_Regression_Part1.npy")
@@ -30,116 +32,100 @@ y_data = np.load(getcwd() + "/regression/training_set/Ytrain_Regression_Part1.np
 
 x_training_set, x_test_set, y_training_set, y_test_set = train_test_split(x_data, y_data, test_size=10)
 
-# Linear model
+'''
+Correlation matrix for input data
+
+'''
+'''
+data = np.c_[ x_data, y_data ] 
+df = pd.DataFrame(data)
+corrMatrix = df.corr()
+heatmap(corrMatrix, annot=True)
+show()
+'''
+
+'''
+Regression models
+
+'''
+
+#x_training_set, x_test_set, y_training_set, y_test_set = train_test_split(x_data, y_data, test_size=10)
 
 linearModel = LinearRegression()
-linearModel.fit(x_training_set,y_training_set)
 
-y_pred_linear = linearModel.predict(x_test_set)
-mse_linear = mean_squared_error(y_test_set, y_pred_linear)    #0.009726150151957687
+XGBoostModel = XGBRegressor()
 
-# XGBoost Regressor
-
-XGBoostModel = XGBRegressor(verbosity=0)
-XGBoostModel.fit(x_training_set, y_training_set)
-
-y_pred_XGBoost = XGBoostModel.predict(x_test_set)
-mse_xgbr = mean_squared_error(y_test_set, y_pred_XGBoost)
-
-# CatBoost Regressor
-'''
 CatBoostModel = CatBoostRegressor(loss_function='RMSE')
-CatBoostModel.fit(x_training_set, y_training_set)
-
-y_pred_CatBoost = CatBoostModel.predict(x_test_set)
-mse_catBoost = mean_squared_error(y_test_set, y_pred_CatBoost)
-'''
-# Stochastic Gradient Descent Regression
 
 SGDRegressorModel = SGDRegressor()
-SGDRegressorModel.fit(x_training_set, np.ravel(y_training_set))
 
-y_pred_SGDRegressor = SGDRegressorModel.predict(x_test_set)
-mse_SGDRegressor = mean_squared_error(y_test_set, y_pred_SGDRegressor)
-
-# Kernel Ridge Regression
-
-KernelRidgeModel = KernelRidge(alpha=1.0)                   # best value is alpha = 0.0
-KernelRidgeModel.fit(x_training_set, y_training_set)
-
-y_pred_KernelRidge = KernelRidgeModel.predict(x_test_set)
-mse_KernelRidge = mean_squared_error(y_test_set, y_pred_KernelRidge)
-
-
-# Elastic Net Regression
+#Best value alpha = 0.0
+KernelRidgeModel = KernelRidge(alpha=1.0)
 
 ElasticNetModel = ElasticNet(random_state=0)
-ElasticNetModel.fit(x_training_set, y_training_set)
 
-y_pred_ElasticNet = ElasticNetModel.predict(x_test_set)
-mse_ElasticNet = mean_squared_error(y_test_set, y_pred_ElasticNet)
-
-# Bayesian Ridge Regression                     better than linear 43 percent of the times
-
+#Better than linear 43 percent of the times
 BayesianRidgeModel = BayesianRidge()
-BayesianRidgeModel.fit(x_training_set, np.ravel( y_training_set))
 
-y_pred_BayesianRidge = BayesianRidgeModel.predict(x_test_set)
-mse_BayesianRidge = mean_squared_error(y_test_set, y_pred_BayesianRidge)
-
-
-# Gradient Boosting Regression
-
-GBRegressorModel = GradientBoostingRegressor(random_state=0)
-GBRegressorModel.fit(x_training_set, np.ravel( y_training_set))
-
-y_pred_GradientBoosting = GBRegressorModel.predict(x_test_set)
-mse_GradientBoosting = mean_squared_error(y_test_set, y_pred_GradientBoosting)
-
-# Support Vector Machine
+GBRegressorModel = GradientBoostingRegressor(learning_rate=0.01, max_depth=1, n_estimators=3000, subsample=0.2)
 
 SVRModel = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=0.2))
-SVRModel.fit(x_training_set, np.ravel( y_training_set))
-
-y_pred_SVR = SVRModel.predict(x_test_set)
-mse_SVR = mean_squared_error(y_test_set, y_pred_SVR)
 
 '''
-print('------------------------------MSE of Regressors------------------------------')
-print('MSE of linear Regressor:         ', mse_linear)
-print('MSE of XGBoost Regressor:        ', mse_xgbr)
-#print('MSE of CatBoost Regressor:       ', mse_catBoost)
-print('MSE of SGD Regressor:            ', mse_SGDRegressor)
-print('MSE of KernelRidge Regressor:    ', mse_KernelRidge)
-print('MSE of ElasticNet Regressor:     ', mse_ElasticNet)
-print('MSE of BayesianRidge Regressor:  ', mse_BayesianRidge)
-print('MSE of GB Regressor:             ', mse_GradientBoosting)
-print('MSE of SVR Regressor:            ', mse_SVR)
+Cross validation
+
 '''
-
-#gam = LinearGAM(n_splines=10).gridsearch(x_data, y_data)
-
-XGBoostModel_score = cross_val_score(XGBoostModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
-lienar_score = cross_val_score(linearModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
-SGDRegressorModel_score = cross_val_score(SGDRegressorModel, x_data, np.ravel(y_data), scoring='neg_mean_squared_error', cv=5)
-KernelRidgeModel_score = cross_val_score(KernelRidgeModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
-ElasticNetModel_score = cross_val_score(ElasticNetModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
-BayesianRidgeModel_score = cross_val_score(BayesianRidgeModel, x_data, np.ravel(y_data), scoring='neg_mean_squared_error', cv=5)
-GBRegressorModel_score = cross_val_score(GBRegressorModel, x_data, np.ravel(y_data), scoring='neg_mean_squared_error', cv=5)
-SVRModel_score = cross_val_score(SVRModel, x_data, np.ravel(y_data), scoring='neg_mean_squared_error', cv=5)
-
-print(lienar_score)
-print(XGBoostModel_score)
-print(SGDRegressorModel_score)
-print(KernelRidgeModel_score)
-print(ElasticNetModel_score)
-print(BayesianRidgeModel_score)
-print(GBRegressorModel_score)
-print(SVRModel_score)
-
-# TODO Find methodes for evaluation data
+'''
+lienar_score                = cross_val_score(linearModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
+XGBoostModel_score          = cross_val_score(XGBoostModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
+SGDRegressorModel_score     = cross_val_score(SGDRegressorModel, x_data, np.ravel(y_data), scoring='neg_mean_squared_error', cv=5)
+KernelRidgeModel_score      = cross_val_score(KernelRidgeModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
+ElasticNetModel_score       = cross_val_score(ElasticNetModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
+BayesianRidgeModel_score    = cross_val_score(BayesianRidgeModel, x_data, np.ravel(y_data), scoring='neg_mean_squared_error', cv=5)
+GBRegressorModel_score      = cross_val_score(GBRegressorModel, x_data, np.ravel(y_data), scoring='neg_mean_squared_error', cv=5)
+SVRModel_score              = cross_val_score(SVRModel, x_data, np.ravel(y_data), scoring='neg_mean_squared_error', cv=5)
+'''
 
 # testXdata = np.load(os.getcwd()+ "/regression/test_set/Xtest_Regression_Part1.npy")
-# Predict y with best model
 # np.save('data.npy', testOutput)
-# Use function to evaluate the format of your submitted data
+
+parameters = {'learning_rate': [0.01,0.02,0.03,0.04, 0.8, 0.1, 0.5],
+              'subsample'    : [1.0 , 0.9, 0.5, 0.2, 0.1],
+              'n_estimators' : [100,500,1000, 1500, 2000, 3000],
+              'max_depth'    : [1,2,3,4,6,8,10]
+             }
+
+
+linearModel.fit(x_training_set, y_training_set)
+y_pred_lin = linearModel.predict(x_test_set)
+print(mean_squared_error(y_test_set, y_pred_lin))
+
+GBRegressorModel.fit(x_training_set, np.ravel(y_training_set))
+y_pred = GBRegressorModel.predict(x_test_set)
+print(mean_squared_error(y_test_set, y_pred))
+
+
+# GBRegressorModel.get_params()
+'''
+             {'alpha': 0.9, 
+             'ccp_alpha': 0.0,
+             'criterion': 'friedman_mse',
+             'init': None,
+             'learning_rate': 0.1,
+             'loss': 'squared_error',
+             'max_depth': 3,
+             'max_features': None,
+             'max_leaf_nodes': None, 
+             'min_impurity_decrease': 0.0, 
+             'min_samples_leaf': 1, 
+             'min_samples_split': 2, 
+             'min_weight_fraction_leaf': 0.0,
+             'n_estimators': 100,
+             'n_iter_no_change': None,
+             'random_state': 0,
+             'subsample': 1.0,
+             'tol': 0.0001,
+             'validation_fraction': 0.1,
+             'verbose': 0,
+             'warm_start': False}
+             '''
