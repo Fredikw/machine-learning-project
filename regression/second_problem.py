dfcedfaf9797434f678caa280b149614517d584e
fredikw <fred.i.kwizera@gmail.com>
2021-10-17 19:17:36 +0200

    Removing outliers Simple solution

---
+++ regression/second_problem.py
@@ -0,0 +1,64 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import ElasticNet
+
+from sklearn.model_selection import cross_val_score
+
+from os import getcwd
+
+# Data load
+x_data = np.load(getcwd() + "/regression/training_set/Xtrain_Regression_Part2.npy")
+y_data = np.load(getcwd() + "/regression/training_set/Ytrain_Regression_Part2.npy")
+
+'''
+Regression models
+
+'''
+
+linearModel = LinearRegression()
+
+#ElasticNetModel = ElasticNet(alpha=0.001, l1_ratio=1)
+
+'''
+Removing outliers from data sample
+
+'''
+
+# MSE of raw data
+linearModel_score = cross_val_score(linearModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
+print(linearModel_score)
+
+linearModel.fit(x_data, y_data)
+
+prediciton          = linearModel.predict(x_data)
+distance_from_mean  = prediciton - y_data
+
+plt.plot(distance_from_mean, 'ro')
+plt.show()
+
+tol = 2
+
+outlier_list = []
+
+for i in range(len(x_data)):
+
+    if abs(distance_from_mean[i]) > tol:
+        outlier_list.append(i)
+
+for i in range(len(outlier_list)):
+
+    outlier = outlier_list[i]
+
+    outlier -= i
+
+    x_data = np.delete(x_data, outlier, 0)
+    y_data = np.delete(y_data, outlier, 0)
+
+# plt.plot(distance_from_mean, 'ro')
+# plt.show()
+
+linearModel_score = cross_val_score(linearModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
+print(linearModel_score)
