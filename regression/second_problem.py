3b92b1e4213bbf34d5259ef1f57f2f44cd25aa0d
fredikw <fred.i.kwizera@gmail.com>
2021-10-23 19:23:35 +0200

    patch-13

--- regression/second_problem.py
+++ regression/second_problem.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 from utils import get_unique_elements
 
 from sklearn.linear_model import LinearRegression
-
-# from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import ElasticNet
 
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import LocalOutlierFactor
@@ -24,7 +23,7 @@ Regression models
 
 linearModel = LinearRegression()
 
-# ElasticNetModel = ElasticNet(alpha=0.001, l1_ratio=1)
+ElasticNetModel = ElasticNet(alpha=0.001, l1_ratio=1)
 
 '''
 Removing outliers from data sample
@@ -73,64 +72,71 @@ for idx, outlier in enumerate(outliers):
 linearModel_score = cross_val_score(linearModel, x_data, y_data, scoring="neg_mean_squared_error", cv=5)
 print(linearModel_score)
 
-
-# # Method 2
-
-# columns = data.shape[1]
-
-# outliers = []
-
-# # Identify outliers
-
-# for idx in range(columns):
-
-#     y_data = data[:, idx]
-#     x_data = np.delete(data, idx, 1)
-
-#     linearModel = LinearRegression()
-#     linearModel.fit(x_data, y_data)
-
-#     prediciton = linearModel.predict(x_data)
-#     distance_from_mean = prediciton - y_data
-
-#     # Method 1: Standard Deviation Method
-
-#     data_mean, data_std = np.mean(distance_from_mean), np.std(distance_from_mean)
-
-#     standard_diviations = 3
-#     cut_off = data_std * standard_diviations
-
-#     for idx, distance in enumerate(distance_from_mean):
-
-#         if abs(distance) > cut_off:
-#             outliers.append(idx)
-
-#     # plt.plot(distance_from_mean, 'ro')
-#     # plt.title('feature ' + str(column))
-#     # plt.show()
-
-# outliers = get_unique_elements(outliers)
-# outliers = np.sort(outliers)
-
-# # Remove outliers
-
-# x_data = np.load(getcwd() + "/training_set/Xtrain_Regression_Part2.npy")
-# y_data = np.load(getcwd() + "/training_set/Ytrain_Regression_Part2.npy")
-
-# for idx, outlier in enumerate(outliers):
-
-#     outlier -= idx
-
-#     x_data = np.delete(x_data, outlier, 0)
-#     y_data = np.delete(y_data, outlier, 0)
-
-# linearModel_score = cross_val_score(linearModel, x_data, y_data, scoring="neg_mean_squared_error", cv=5)
-# print(linearModel_score)
+ElasticNetModel_score = cross_val_score(ElasticNetModel, x_data, y_data, scoring="neg_mean_squared_error", cv=5)
+print(ElasticNetModel_score)
 
 
-# # Method 2 with the Standard Deviation Method results
-# # in the following outliers
-# # [ 7 23 36 42 78 85 92]
+# Method 2
+
+columns = data.shape[1]
+
+outliers = []
+
+# Identify outliers
+
+for idx in range(columns):
+
+    y_data = data[:, idx]
+    x_data = np.delete(data, idx, 1)
+
+    linearModel = LinearRegression()
+    linearModel.fit(x_data, y_data)
+
+    prediciton = linearModel.predict(x_data)
+    distance_from_mean = prediciton - y_data
+
+    # Method 1: Standard Deviation Method
+
+    data_mean, data_std = np.mean(distance_from_mean), np.std(distance_from_mean)
+
+    standard_diviations = 3
+    cut_off = data_std * standard_diviations
+
+    for idx, distance in enumerate(distance_from_mean):
+
+        if abs(distance) > cut_off:
+            outliers.append(idx)
+
+    # plt.plot(distance_from_mean, 'ro')
+    # plt.title('feature ' + str(column))
+    # plt.show()
+
+outliers = get_unique_elements(outliers)
+outliers = np.sort(outliers)
+
+# Remove outliers
+
+x_data = np.load(getcwd() + "/training_set/Xtrain_Regression_Part2.npy")
+y_data = np.load(getcwd() + "/training_set/Ytrain_Regression_Part2.npy")
+
+for idx, outlier in enumerate(outliers):
+
+    outlier -= idx
+
+    x_data = np.delete(x_data, outlier, 0)
+    y_data = np.delete(y_data, outlier, 0)
+
+
+linearModel_score = cross_val_score(linearModel, x_data, y_data, scoring="neg_mean_squared_error", cv=5)
+print(linearModel_score)
+
+ElasticNetModel_score = cross_val_score(ElasticNetModel, x_data, y_data, scoring="neg_mean_squared_error", cv=5)
+print(ElasticNetModel_score)
+
+
+# Method 2 with the Standard Deviation Method results
+# in the following outliers
+# [ 7 23 36 42 78 85 92]
 
 
 # Methode 3
