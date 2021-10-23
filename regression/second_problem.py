0d4b43f7c2856f1bd2c4f3d1833f9702d5860375
fredikw <fred.i.kwizera@gmail.com>
2021-10-23 19:57:36 +0200

    Finish part 2

--- regression/second_problem.py
+++ regression/second_problem.py
@@ -18,16 +18,18 @@ from seaborn import heatmap
 x_data = np.load(getcwd() + "/training_set/Xtrain_Regression_Part2.npy")
 y_data = np.load(getcwd() + "/training_set/Ytrain_Regression_Part2.npy")
 
+data = np.c_[x_data, y_data]
+
 '''
 Correlation matrix for input data
 
 '''
 
-data = np.c_[ x_data, y_data ] 
-df = pd.DataFrame(data)
-corrMatrix = df.corr()
-heatmap(corrMatrix, annot=False)
-plt.show()
+# data = np.c_[ x_data, y_data ] 
+# df = pd.DataFrame(data)
+# corrMatrix = df.corr()
+# heatmap(corrMatrix, annot=False)
+# plt.show()
 
 '''
 Regression models
@@ -47,48 +49,47 @@ Removing outliers from data sample
 # linearModel_score = cross_val_score(linearModel, x_data, y_data, scoring='neg_mean_squared_error', cv=5)
 # print(linearModel_score)
 
-data = np.c_[x_data, y_data]
 
 
-# Method 1
+# # Method 1
 
-linearModel.fit(x_data, y_data)
+# linearModel.fit(x_data, y_data)
 
-prediciton = linearModel.predict(x_data)
-distance_from_mean = prediciton - y_data
+# prediciton = linearModel.predict(x_data)
+# distance_from_mean = prediciton - y_data
 
-# plt.plot(distance_from_mean, 'ro')
-# plt.show()
+# # plt.plot(distance_from_mean, 'ro')
+# # plt.show()
 
-data_std = np.std(distance_from_mean)
+# data_std = np.std(distance_from_mean)
 
-standard_diviations = 2
-cut_off = data_std * standard_diviations
+# standard_diviations = 2
+# cut_off = data_std * standard_diviations
 
-outliers = []
+# outliers = []
 
-for idx, distance in enumerate(distance_from_mean):
+# for idx, distance in enumerate(distance_from_mean):
 
-    if abs(distance) > cut_off:
-        outliers.append(idx)
+#     if abs(distance) > cut_off:
+#         outliers.append(idx)
 
-outliers = get_unique_elements(outliers)
-outliers = np.sort(outliers)
+# outliers = get_unique_elements(outliers)
+# outliers = np.sort(outliers)
 
-for idx, outlier in enumerate(outliers):
+# for idx, outlier in enumerate(outliers):
 
-    outlier -= idx
+#     outlier -= idx
 
-    x_data = np.delete(x_data, outlier, 0)
-    y_data = np.delete(y_data, outlier, 0)
+#     x_data = np.delete(x_data, outlier, 0)
+#     y_data = np.delete(y_data, outlier, 0)
 
-# Testing
+# # Testing
 
-linearModel_score = cross_val_score(linearModel, x_data, y_data, scoring="neg_mean_squared_error", cv=5)
-print(linearModel_score)
+# linearModel_score = cross_val_score(linearModel, x_data, y_data, scoring="neg_mean_squared_error", cv=10)
+# print(linearModel_score)
 
-ElasticNetModel_score = cross_val_score(ElasticNetModel, x_data, y_data, scoring="neg_mean_squared_error", cv=5)
-print(ElasticNetModel_score)
+# ElasticNetModel_score = cross_val_score(ElasticNetModel, x_data, y_data, scoring="neg_mean_squared_error", cv=10)
+# print(ElasticNetModel_score)
 
 
 # Method 2
@@ -141,14 +142,13 @@ for idx, outlier in enumerate(outliers):
     x_data = np.delete(x_data, outlier, 0)
     y_data = np.delete(y_data, outlier, 0)
 
-
 # Testing
 
-linearModel_score = cross_val_score(linearModel, x_data, y_data, scoring="neg_mean_squared_error", cv=5)
-print(linearModel_score)
+# linearModel_score = cross_val_score(linearModel, x_data, y_data, scoring="neg_mean_squared_error", cv=10)
+# print(linearModel_score)
 
-ElasticNetModel_score = cross_val_score(ElasticNetModel, x_data, y_data, scoring="neg_mean_squared_error", cv=5)
-print(ElasticNetModel_score)
+# ElasticNetModel_score = cross_val_score(ElasticNetModel, x_data, y_data, scoring="neg_mean_squared_error", cv=10)
+# print(ElasticNetModel_score)
 
 
 # Method 2 with the Standard Deviation Method results
@@ -159,3 +159,14 @@ print(ElasticNetModel_score)
 # Methode 3
 
 # Unsupervised Outlier Detection using Local Outlier Factor
+
+
+'''
+Submission
+
+'''
+
+ElasticNetModel.fit(x_data, y_data)
+x_data_test = np.load(getcwd()+ "/test_set/Xtest_Regression_Part2.npy")
+test_output = ElasticNetModel.predict(x_data_test)
+np.save('test_set_predictions.npy', test_output)
