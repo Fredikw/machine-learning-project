20629e433a23a7bc737d02817db0ab81c8b705f3
fredikw <fred.i.kwizera@gmail.com>
2021-10-26 13:07:53 +0200

    patch-1

--- classification/first_classification_task.py
+++ classification/first_classification_task.py
@@ -1,1 +1,65 @@
-print("hello Gunnar")
+import numpy as np
+import matplotlib.pyplot as plt
+
+from os import getcwd
+
+'''
+Loading data
+
+'''
+
+x_data = np.load(getcwd() + "/training_set/Xtrain_Classification_Part1.npy")
+y_data = np.load(getcwd() + "/training_set/Ytrain_Classification_Part1.npy")
+
+print(x_data.shape)
+print(y_data.shape)
+
+
+'''
+Show image
+
+'''
+
+# def imshow(img):
+#     img = np.reshape(np.array(img),(50,50))
+#     plt.imshow(img,cmap='gray')
+#     plt.show()
+
+# imshow(x_data[2032])
+
+'''
+confusion matrix
+
+'''
+
+'''
+Utility function
+
+'''
+
+def to_one_hot_enc(arr):
+
+    one_hot_enc = []
+
+    for element in arr:
+
+        if arr:
+            one_hot_enc.append(np.array([1,0]))
+        else:
+            one_hot_enc.append(np.array([0,1]))
+    
+    return one_hot_enc
+
+
+def from_one_hot_enc(arr):
+
+    not_one_hot_enc = []
+    
+    for element in arr:
+
+        if element[0]:
+            not_one_hot_enc.append(1)
+        else:
+            not_one_hot_enc.append(0)
+    
+    return not_one_hot_enc
--- classification/second_classification_task.py
+++ classification/second_classification_task.py
@@ -1,13 +1,1 @@
-import numpy as np
-import matplotlib as plt
-
-
-
-'''
-Confusion matrix
-
-'''
-
-def imshow(img):
-    img = img.nmpy().reshape(50,50)
-    plt.imshow(img='groy')
+print('hello Gunnar')
