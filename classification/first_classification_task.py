0f3e094fb788fbb0c499ea1c3931957aee54c1cf
fredikw <fred.i.kwizera@gmail.com>
2021-10-27 13:16:32 +0200

    Added SGD classifier

--- classification/first_classification_task.py
+++ classification/first_classification_task.py
@@ -1,6 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
+from sklearn.linear_model import SGDClassifier
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
+
+from utils import *
 from os import getcwd
 
 '''
@@ -8,17 +15,18 @@ Loading data
 
 '''
 
-x_data = np.load(getcwd() + "/training_set/Xtrain_Classification_Part1.npy")
-y_data = np.load(getcwd() + "/training_set/Ytrain_Classification_Part1.npy")
 
-print(x_data.shape)
-print(y_data.shape)
+x_data = np.load(getcwd() + "/training_set/Xtrain_Classification_Part1.npy") # (6513, 2500)
+y_data = np.load(getcwd() + "/training_set/Ytrain_Classification_Part1.npy") # (6513,)
+
+x_training_set, x_test_set, y_training_set, y_test_set = train_test_split(x_data, y_data, test_size=0.10)
 
 
 '''
 Show image
 
 '''
+
 
 # def imshow(img):
 #     img = np.reshape(np.array(img),(50,50))
@@ -28,38 +36,26 @@ Show image
 # imshow(x_data[2032])
 
 '''
+Classification models
+
+'''
+
+
+SGD = SGDClassifier(max_iter = 1000, tol=1e-3,penalty = "elasticnet")
+
+# SGD.fit(x_training_set, y_training_set)
+
+# predict = SGD.predict(x_test_set)
+
+# # With cross validation
+
+# SGD_score = cross_val_score(SGD, x_data, y_data, scoring='accuracy', cv=5)
+
+# print(SGD_score)
+
+
+'''
 confusion matrix
 
 '''
 
-'''
-Utility function
-
-'''
-
-def to_one_hot_enc(arr):
-
-    one_hot_enc = []
-
-    for element in arr:
-
-        if arr:
-            one_hot_enc.append(np.array([1,0]))
-        else:
-            one_hot_enc.append(np.array([0,1]))
-    
-    return one_hot_enc
-
-
-def from_one_hot_enc(arr):
-
-    not_one_hot_enc = []
-    
-    for element in arr:
-
-        if element[0]:
-            not_one_hot_enc.append(1)
-        else:
-            not_one_hot_enc.append(0)
-    
-    return not_one_hot_enc
