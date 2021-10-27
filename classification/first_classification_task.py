48d3ae5644f32a0b0b856d0a17d67410104726de
fredikw <fred.i.kwizera@gmail.com>
2021-10-27 14:39:51 +0200

    Added confusion matrix

--- classification/first_classification_task.py
+++ classification/first_classification_task.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 # import matplotlib.pyplot as plt
 
 from sklearn.linear_model import SGDClassifier
@@ -6,9 +7,10 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 
-
 from utils import *
 from os import getcwd
+from seaborn import heatmap
+from matplotlib.pyplot import show
 
 '''
 Loading data
@@ -43,10 +45,6 @@ Classification models
 
 SGD = SGDClassifier(max_iter = 1000, tol=1e-3,penalty = "elasticnet")
 
-# SGD.fit(x_training_set, y_training_set)
-
-# predict = SGD.predict(x_test_set)
-
 # # With cross validation
 
 # SGD_score = cross_val_score(SGD, x_data, y_data, scoring='accuracy', cv=5)
@@ -59,3 +57,26 @@ confusion matrix
 
 '''
 
+
+SGD.fit(x_training_set, y_training_set)
+
+predict_training  = SGD.predict(x_training_set)
+predict_test      = SGD.predict(x_test_set)
+
+# print(confusion_matrix(y_training_set, predict_training, labels=[0.0, 1.0]))
+# print(confusion_matrix(y_test_set, predict_test, labels=[0.0, 1.0]))
+
+data_training = {'Target training': y_training_set, 'Predicted training': predict_training}
+data_test     = {'Target test': y_test_set, 'Predicted test': predict_test}
+
+df_training   = pd.DataFrame(data_training, columns=['Target training','Predicted training'])
+df_test       = pd.DataFrame(data_test, columns=['Target test','Predicted test'])
+
+confusion_matrix_training  = pd.crosstab(df_training['Target training'], df_training['Predicted training'], rownames=['Target class'], colnames=['Output class'], margins = True, normalize=True)
+confusion_matrix_test      = pd.crosstab(df_test['Target test'], df_test['Predicted test'], rownames=['Target class'], colnames=['Output class'], margins = True, normalize=True)
+
+
+heatmap(confusion_matrix_training, annot=True)
+show()
+heatmap(confusion_matrix_test, annot=True)
+show()
