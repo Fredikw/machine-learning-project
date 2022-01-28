d90cfbc26c83eba2144429518dfc0c38c9a76e32
fredikw <fred.i.kwizera@gmail.com>
2022-01-28 14:39:13 +0100

    Housekeeping

--- regression/first_problem.py
+++ regression/first_problem.py
@@ -2,23 +2,23 @@ import numpy as np
 import pandas as pd
 import  matplotlib.pyplot as plt
 
-from xgboost import XGBRegressor
-from catboost import CatBoostRegressor
+# from xgboost import XGBRegressor
+# from catboost import CatBoostRegressor
 
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.kernel_ridge import KernelRidge
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import SGDRegressor
 from sklearn.linear_model import ElasticNet
-from sklearn.linear_model import BayesianRidge
-from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
+# from sklearn.ensemble import GradientBoostingRegressor
+# from sklearn.kernel_ridge import KernelRidge
+# from sklearn.linear_model import SGDRegressor
+# from sklearn.linear_model import BayesianRidge
+# from sklearn.svm import SVR
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_squared_error
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.pipeline import make_pipeline
+# from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import cross_val_score
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.metrics import mean_squared_error
 #from sklearn.metrics import mean_squared_error
 #from sklearn.model_selection import train_test_split
 
